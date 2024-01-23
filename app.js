fc209922eefa1ec7e698c14adf5b2369fa51f534
Erick Rodrigues <erkrodrigs@gmail.com>
2024-01-23 17:37:01 -0300

    adiciona o titulo do site

---
+++ app.js
@@ -0,0 +1,7 @@
+function tituloDoSite() {
+    let greeting = document.querySelector('h1');
+    greeting.innerHTML = 'Bem-vindo(a) ao Decodificador de Texto!';
+    return greeting;
+}
+
+tituloDoSite();
